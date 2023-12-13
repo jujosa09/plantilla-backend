6cd3bea6a054e125438a386c9cfcff62a1bf98e6
jujosa09 <jujosadols@uma.es>
2023-12-13 20:02:02 +0100

    update cors options

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors())
 app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 
