043f9a7cbb18f08e4ef9e93da5095d8b4d57b824
jujosa09 <jujosadols@uma.es>
2023-12-15 15:41:30 +0100

    changed database mongo

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(`mongodb+srv://jujosadols:${process.env.MONGO_PASSWD}@cluster0.kc9dnvs.mongodb.net/pruebas?retryWrites=true&w=majority`) 
+mongoose.connect(`mongodb+srv://jujosadols:${process.env.MONGO_PASSWD}@cluster0.kc9dnvs.mongodb.net/examen?retryWrites=true&w=majority`) 
 
 .then(() => {
     console.log('Connected to Mongo')
